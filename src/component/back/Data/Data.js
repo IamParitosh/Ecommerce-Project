58982b44635b47f080cf89dc5ab3182f835d64b5
Paritosh Gupta <65158930+IamParitosh@users.noreply.github.com>
2021-11-26 03:57:37 +0530

    Add files via upload

---
+++ src/component/back/Data/Data.js
@@ -0,0 +1,54 @@
+const data =  {
+    productItems:[
+        {
+            id:"1",
+            name: "Headphone",
+            price: 999,
+            image: "./pics/headphone.jpg"
+        },
+        {
+            id:"2",
+            name: "Mobile",
+            price: 10999,
+            image: "./pics/Mobile.jpg"
+        },
+        {
+            id:"3",
+            name: "Laptop",
+            price: 22999,
+            image: "./pics/Laptop.jpg"
+        },
+        {
+            id:"4",
+            name: "Speaker",
+            price: 6999,
+            image: "./pics/Speaker.jpg"
+        },
+        {
+            id:"5",
+            name: "Smartwatch",
+            price: 10999,
+            image: "./pics/Smartwatch.jpg"
+        },
+        {
+            id:"6",
+            name: "Airdopes",
+            price: 4999,
+            image: "./pics/Airdope.jpg"
+        },
+        {
+            id:"7",
+            name: "AC",
+            price: 15999,
+            image: "./pics/AC.jpg"
+        },
+        {
+            id:"8",
+            name: "Cooler",
+            price: 8999,
+            image: "./pics/Cooler.jpg"
+        }
+    
+    ]
+}
+export default data;
